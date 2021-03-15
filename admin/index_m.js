7767c3db2674ba1bb7b9554f087a741adfd6f60d
hombach <63002482+hombach@users.noreply.github.com>
2021-03-15 21:44:45 +0100

    0.0.4

--- admin/index_m.js
+++ admin/index_m.js
@@ -42,25 +42,6 @@ function load(settings, onChange) {
         });
     });
 
-
-    $('#actualValueTempDialogPopUp').on('click', function () {
-        initSelectId(function (sid) {
-            sid.selectId('show', $('#actualValueTemp').val(), function (newId) {
-                if (newId) {
-                    $('#actualValueTemp').val(newId).trigger('change');
-                }
-            });
-        });
-    });
-
-
-
-
-
-
-
-
-
     $('#StateHomePowerConsumptionPopUp').on('click', function () {
         initSelectId(function (sid) {
             sid.selectId('show', $('#StateHomePowerConsumption').val(), function (newId) {
@@ -118,113 +99,42 @@ function save(callback) {
     callback(obj);
 }
 
-// NEW from BlueFox, Not Working
-// var selectId;
-function WRONGinitSelectId(callback) {
-    window.alert("function initSelectId entered");
-    if (selectId) return callback(selectId);
-    window.alert("function initSelectId entered 2");
-    socket.emit('getObjects', function (err, objs) {
-        selectId = $('#dialog-select-member').selectId('init', {
-            noMultiselect: true,
-            objects: objs,
-            imgPath: '../../lib/css/fancytree/',
-            name: 'select-foreign-state',
-        });
-        window.alert("Error: " + err);
-        window.alert(objs);
-        callback(selectId);
-    });
-}
-// END NEW 
 
-
-/*
- ## How to use
-    ```
- <!-- Somewhere in HTML -->
-    <div id="dialog-select-member" style="display: none"></div>
- ```
-
-    ```
- // In Javascript
- // Name "dialog-select-member" is important, because for that exist the CSS classes
- // Important to have "admin/img/big-info.png" in too, because this icon will be loaded if no icon found, elsewise we have endless loop
- var selectId;
- function initSelectId (cb) {
-     if (selectId) return cb ? cb(selectId) : selectId;
-    socket.emit('getObjects', function (err, res) {
-        if (!err && res) {
-            selectId = $('#dialog-select-member').selectId('init',  {
-                noMultiselect: true,
-                objects: res,
-                imgPath:       '../../lib/css/fancytree/',
-                filter:        {type: 'state'},
-                name:          'adapter-select-state',
-                texts: {
-                    select:          _('Select'),
-                    cancel:          _('Cancel'),
-                    all:             _('All'),
-                    id:              _('ID'),
-                    name:            _('Name'),
-                    role:            _('Role'),
-                    room:            _('Room'),
-                    value:           _('Value'),
-                    selectid:        _('Select ID'),
-                    from:            _('From'),
-                    lc:              _('Last changed'),
-                    ts:              _('Time stamp'),
-                    wait:            _('Processing...'),
-                    ack:             _('Acknowledged'),
-                    selectAll:       _('Select all'),
-                    unselectAll:     _('Deselect all'),
-                    invertSelection: _('Invert selection')
-                },
-                columns: ['image', 'name', 'role', 'room', 'value']
-            });
-            cb && cb(selectId);
-        }
-    });
-}
-*/
 let selectId;
 function initSelectId(callback) {
-    window.alert("function initSelectId entered");
-    if (selectId) {
-        return callback(selectId);
-    }
-    window.alert("function initSelectId entered 2");
+    if (selectId) return callback(selectId);
+window.alert("function initSelectId entered");
     socket.emit('getObjects', function (err, objs) {
-        selectId = $('#dialog-select-member').selectId('init', {
- //       selectId = $('#adapter-container').selectId('init', {
-            noMultiselect: true,
-            objects: objs,
-            imgPath: '../../lib/css/fancytree/',
-            filter: { type: 'state' },
-            name: 'scenes-select-state',
-            texts: {
-                select: _('Select'),
-                cancel: _('Cancel'),
-                all: _('All'),
-                id: _('ID'),
-                name: _('Name'),
-                role: _('Role'),
-                room: _('Room'),
-                value: _('Value'),
-                selectid: _('Select ID'),
-                from: _('From'),
-                lc: _('Last changed'),
-                ts: _('Time stamp'),
-                wait: _('Processing...'),
-                ack: _('Acknowledged'),
-                selectAll: _('Select all'),
-                unselectAll: _('Deselect all'),
-                invertSelection: _('Invert selection')
-            },
-            columns: ['image', 'name', 'role', 'room']
-        });
-        window.alert("Error: " + err);
-        window.alert("Objects: " + objs);
-        callback(selectId);
+        if (!err && objs) {
+            selectId = $('#dialog-select-member').selectId('init', {
+                noMultiselect: true,
+                objects: objs,
+                imgPath: '../../lib/css/fancytree/',
+                filter: { type: 'state' },
+                name: 'adapter-select-state',
+                texts: {
+                    select: _('Select'),
+                    cancel: _('Cancel'),
+                    all: _('All'),
+                    id: _('ID'),
+                    name: _('Name'),
+                    role: _('Role'),
+                    room: _('Room'),
+                    value: _('Value'),
+                    selectid: _('Select ID'),
+                    from: _('From'),
+                    lc: _('Last changed'),
+                    ts: _('Time stamp'),
+                    wait: _('Processing...'),
+                    ack: _('Acknowledged'),
+                    selectAll: _('Select all'),
+                    unselectAll: _('Deselect all'),
+                    invertSelection: _('Invert selection')
+                },
+                columns: ['image', 'name', 'role', 'room']
+            });
+window.alert("Error: " + err + "  Objects: " + objs);
+            callback(selectId);
+        }
     });
 }

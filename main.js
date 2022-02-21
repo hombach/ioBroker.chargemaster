2f3a49ef3bfc3038dcf02526455962ad7f0bdcc0
hombach <63002482+hombach@users.noreply.github.com>
2022-02-21 23:35:27 +0100

    0.4.1

--- main.js
+++ main.js
@@ -247,7 +247,10 @@ class chargemaster extends utils.Adapter {
             }
 
             else if (Wallbox[i].ChargeManager) { // Charge-Manager is enabled for this wallbox
-                BatSoC = Number(this.asyncGetForeignStateVal(this.config.StateHomeBatSoc));
+                BatSoC = this.asyncGetForeignStateVal(this.config.StateHomeBatSoc);
+                //BatSoC = Number(this.asyncGetForeignStateVal(this.config.StateHomeBatSoc));
+                //BatSoC = await this.asyncGetForeignStateVal(this.config.StateHomeBatSoc);
+                //BatSoC = Number(this.getForeignStates(this.config.StateHomeBatSoc));
                 this.log.debug(`Got external state of battery SoC: ${BatSoC}%`);
                 if (BatSoC >= MinHomeBatVal) { // SoC of home battery sufficient?
                     this.Charge_Manager(i);

758222bf3bd56196df77d865d6b3e1893f0c84bf
Hombach <GIT@homba.ch>
2024-04-16 00:57:27 +0200

    0.9.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,8 @@
 // eslint.config.js
 module.exports = {
 	// root: true, // Don't look outside this project for inherited configs
-    parserOptions: {
-		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    },
     extends: [
-        "eslint:recommended",
-        "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        "eslint:recommended"
     ],
     plugins: [],
     env: {
@@ -43,5 +39,8 @@ module.exports = {
 				endOfLine: "auto",
 			},
 		]
+    },
+    parserOptions: {
+		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     },
 }
